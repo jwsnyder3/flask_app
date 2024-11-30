167611f00c897e5df736a642f3f5017c89adc5de
jwsnyder3 <161400606+jwsnyder3@users.noreply.github.com>
2024-11-30 00:05:48 -0600

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// Script used in login
 function toggleAdminLogin() {
     const usernameField = document.getElementById('username');
     const passwordField = document.getElementById('password');
@@ -31,3 +32,47 @@ function showLoginForm() {
     document.getElementById('account-creation-form').style.display = 'none';
     document.getElementById('login-form').style.display = 'block';
 }
+
+// Script used in cart
+document.querySelectorAll('.image-button').forEach(button => {
+    button.addEventListener('click', (event) => {
+        const action = event.target.closest('button').getAttribute('data-action');
+        const itemId = event.target.closest('button').getAttribute('data-item-id');
+        
+        
+        fetch('/update_cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                item_id: itemId,
+                action: action
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+      
+                document.getElementById(`quantity-${itemId}`).textContent = data.new_quantity;
+                document.getElementById('total-price').textContent = data.new_total_price;
+            } else {
+                alert('Error updating cart');
+            }
+        });
+    });
+});
+
+// Script used in Checkout
+var acc = document.getElementsByClassName("accordion");
+        for (var i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+            });
+        }
